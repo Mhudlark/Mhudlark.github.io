c7fa242a46468bd97e9778c62f7dc66cfbc844fe
Hugh Henry <hughcthenry@gmail.com>
2022-11-02 00:19:27 +1100

    Update about content

--- src/data/content/about.ts
+++ src/data/content/about.ts
@@ -16,7 +16,7 @@ const aboutContent = {
 	previousExperience: {
 		subtitle: `My work`,
 		text: [
-			`I'm just getting started but so far I've worked on consulting projects
+			`I've worked on consulting projects
 			with 15+ people on the team, and I've also worked on smaller projects where
 			I regularly interacted with clients directly.`,
 			`Please have a look at my linkedIn if you'd like to know more.`,
