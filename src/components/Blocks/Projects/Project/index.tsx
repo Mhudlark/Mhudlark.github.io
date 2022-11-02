d4f59021411f6e674c1b9defaa6dddb5d9de00b8
Hugh Henry <hughcthenry@gmail.com>
2022-11-02 11:30:43 +1100

    Add projects link icon and update project styling

--- src/components/Blocks/Projects/Project/index.tsx
+++ src/components/Blocks/Projects/Project/index.tsx
@@ -22,9 +22,13 @@ const Project = ({ project }: Props) => {
 		<div className="project">
 			<div className="name-container">
 				<Typography variant={TypographyVariants.h3}>{project.subtitle}</Typography>
-				<a className="project-url" href={project.url} target="_blank" rel="noopener">
-					{project.url}
-				</a>
+				<a
+					className="project-url"
+					href={project.url}
+					title={project.subtitle}
+					target="_blank"
+					rel="noopener"
+				/>
 			</div>
 			<div className="project-content-container">
 				<Typography>{project.text}</Typography>
--- src/data/content/projects.ts
+++ src/data/content/projects.ts
@@ -8,29 +8,11 @@ const projectsContent = {
 				url: './assets/images/projects/mlvet/MLVET.png',
 			},
 			text: [
-				`- Desktop video editing program (Windows, MacOS, Linux)`,
-				`- ML Cloud and Local transcription of video content`,
+				`- Desktop video editing program`,
+				`- ML cloud and local transcription of video content`,
 				`- Edit the transcription to edit the output video`,
-				`- Live output video preview`,
+				`- Live preview of output video`,
 				`- Export tp MP4 and EDL file formats`,
-			],
-		},
-		{
-			subtitle: `Project 2 blah blah`,
-			url: 'https://www.google.com',
-			image: {
-				url: '../../../../static/assets/images/projects/mlvet/MLVET.png',
-			},
-			text: [
-				`- Desktop video editing program (Windows, MacOS, Linux)`,
-				`- ML Cloud and Local transcription of video content`,
-				`- Edit the transcription to edit the output video`,
-				`- Live output video preview`,
-				`- Export tp MP4 and EDL file formats`,
-				`Users are able to transcribe video content using a freemium cloud 
-				transcription service or completely free using local transcription 
-				methods. and then edit the 
-				corresponding transcription to edit the output video.`,
 			],
 		},
 	],
