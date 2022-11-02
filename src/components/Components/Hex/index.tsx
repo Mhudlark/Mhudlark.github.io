ef738521870d6f7046b74737aed4239e1117d2be
Hugh Henry <hughcthenry@gmail.com>
2022-11-02 17:08:57 +1100

    Update empty hexagons to look 3d

--- src/components/Components/Hex/index.tsx
+++ src/components/Components/Hex/index.tsx
@@ -9,6 +9,11 @@ interface Props {
 const Hex = ({ children, style }: Props) => {
 	return (
 		<div className="hex-outer" style={style}>
+			<div className="shadow-bottom-1" />
+			<div className="shadow-bottom-2" />
+			<div className="shadow-right-1" />
+			<div className="shadow-right-2" />
+			<div className="shadow-right-3" />
 			<div className="hex-inner">{children}</div>
 		</div>
 	);
